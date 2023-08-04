20d3762eafea642f253d7def3f7e9e6d2bbe91ac
zhandosbaimurat <z.baimurat@strongte.am>
2023-08-05 02:51:40 +0600

    init

---
+++ app/src/main/java/kz/zhandos/weatherandroid/MainActivity.kt
@@ -0,0 +1,13 @@
+package kz.zhandos.weatherandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kz.zhandos.lib.coreui.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
